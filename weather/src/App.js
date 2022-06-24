6331558ef803a3f35a41a258507e863a617c0444
Evgeniya Kudinova <evgenyakott2728@gmail.com>
2022-06-24 19:50:59 +0400

    Сделали текущий город&дату%время

--- weather/src/App.js
+++ weather/src/App.js
@@ -10,38 +10,47 @@ const API = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lo
 
 
 function App() {
-/*хуко состояние для вывода пасмурности: нач состояние,начальное знач."--------" */
-const [cloudDescription, setcloudDescription] = useState("---------");
+  /*хуко состояние для вывода пасмурности: нач состояние,начальное знач."--------" */
+  const [cloudInfo, setcloudInfo] = useState("---------");
+  const [cloudCity, setcloudCity] = useState("---------");
+  const [cloudTemp, setcloudTemp] = useState("---------");
+  
+    useEffect(() => {
+      /*Асинхронный(одновременный) запрос на получение данных*/
+      const gettingWeather = async () =>{
+        /*fetch - метод, позволяющий полностью прочитать url адрес и получить данные*/
+        const api_url = await fetch(API);
 
+        if (api_url.ok) {
+          /*переводим данные в json формат*/
+        const data = await api_url.json();
+        /*выводим все то, что получили с url адреса*/
+        console.log("Данные, которые получены в формате json",data);
+        setcloudInfo(data.weather[0].description) /*получаем пасмурно, облачно ...*/
+        setcloudCity(data.name)
+        setcloudTemp(data.main.temp)
 
-  useEffect(() => {
-    /*Асинхронный(одновременный) запрос на получение данных*/
-    const gettingWeather = async () =>{
-      /*fetch - метод, позволяющий полностью прочитать url адрес и получить данные*/
-      const api_url = await fetch(API);
-
-      if (api_url.ok) {
-        /*переводим данные в json формат*/
-      const data = await api_url.json();
-      /*выводим все то, что получили с url адреса*/
-      console.log("Данные, которые получены в формате json",data);
-      setcloudDescription(data.weather[0].description) /*получаем пасмурно, облачно ...*/
-      //console.log(cloudDescription);
+        }
+        else{
+          console.error("Возникла проблема с получением данных о погоде");
+        }
       }
-      else{
-        console.error("Возникла проблема с получением данных о погоде");
-      }
-    }
-    gettingWeather()
-  })
+      gettingWeather()
+    })
 
 
-  return (
-    <div className="App">
-      <h2>Привет</h2>
-      {cloudDescription}
-    </div>
-  );
+    return (
+      <div className="App">
+        {/*Текущее*/}
+        <div className="current">
+
+          <div className = "currentCity">{cloudCity}</div>
+          <div className = "currentTemp">{Math.round(cloudTemp)}°</div>
+          <div className = "currentInfo">{cloudInfo}</div>
+
+        </div>
+      </div>
+    );
 }
 
 export default App;
