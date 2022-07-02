f331070f2e7211ab123ada3eeaefb8f76af58c44
Evgeniya Kudinova <evgenyakott2728@gmail.com>
2022-07-03 01:47:00 +0400

    Сделали 7-ми дневный прогноз

--- weather/src/components/DayWeather/DayWeather.js
+++ weather/src/components/DayWeather/DayWeather.js
@@ -7,30 +7,33 @@ function DayWeather({dt, icon, tempDay, tempNight}) {
     let myDate = new Date(dt*1000);
     let thisDay = `${myDate.getDate()}`
     let iconDay = `http://openweathermap.org/img/wn/${icon}@2x.png`
-    let nameDay = myDate.getDate()
+    let nameDay = myDate.getDay()
     console.log(dt)
 
     switch(nameDay){
       case 0:
-        nameDay = "Чт"
+        nameDay = 'Вс'
         break;
       case 1:
-        nameDay = "Пт"
+        nameDay = 'Пн'
         break;
       case 2:
-        nameDay = "Сб"
+        nameDay = 'Вт'
         break;
       case 3:
-        nameDay = "Вс"
+        nameDay = 'Ср'
         break;
       case 4:
-        nameDay = "Пн"
+        nameDay = 'Чт'
         break;
       case 5:
-        nameDay = "Вт"
+        nameDay = 'Пт'
         break;
       case 6:
-        nameDay = "Ср"
+        nameDay = 'Сб'
+        break;
+      case 7:
+        nameDay = 'Вс'
         break;
     }
 
@@ -41,10 +44,10 @@ function DayWeather({dt, icon, tempDay, tempNight}) {
     console.log(thisDay)
 
     return (
-      <div className="DayWeather">
+      <div className='DayWeather'>
         <div className='DayTitle'>
-          <span className="DayWeather_nameDay">{nameDay}</span>
-          <span className="DayWeather_title">{thisDay}</span>
+          <span className='DayWeather_nameDay'>{nameDay}</span>
+          <span className='DayWeather_title'>{thisDay}</span>
         </div>
         <img src={iconDay} />
         <div className='DayWeather_temp'>
